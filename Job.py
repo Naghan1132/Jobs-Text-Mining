d6e2308d6ec031f751a7eb1993cd3b738752bebf
Naghan1132 <grimault52@gmail.com>
2023-11-30 01:03:02 +0100

    dodo

--- Job.py
+++ Job.py
@@ -1,9 +1,10 @@
 class Job:
-    def __init__(self, title,type,description, salary, location, company):
+    def __init__(self, title,type,description, salary, location, company, source):
         self.title = title
         self.type = type # CDD, CDI etc...
         self.description = description
         self.salary = salary
         self.location = location
         self.company = company
+        self.source = source # indeed, apec, etc...
     
