ee4f5aee347cd3ecf84c0e99308686eb7bbb0cc8
Hugo André <145919293+HugoA-A@users.noreply.github.com>
2023-12-06 17:52:01 +0100

    Add files via upload
    Création de la bdd et ajout des valeurs dans : H_type_job, H_salaire et D_entreprise depuis apec.csv

--- SQLite/SQLite.py
+++ SQLite/SQLite.py
@@ -40,7 +40,7 @@ class data_base:
         self.create_entreprise()
         self.create_date()
         self.create_description()
-        self.create_source()
+        #self.create_source()
         self.create_location()
         self.create_titre()
     
@@ -49,13 +49,13 @@ class data_base:
     def create_type_job(self):
         # self.cur.execute("""DROP TABLE H_type_job""")
         self.cur.execute("""CREATE TABLE IF NOT EXISTS H_type_job(
-                         id_type_job INTEGER PRIMARY KEY,
+                         id_type_job INTEGER PRIMARY KEY AUTOINCREMENT,
                          type TEXT                                    
         )""")
     
     # Insert value into H_type_job
     def insert_type_job(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO H_type_job VALUES(?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO H_type_job (type) VALUES(?)""", item)
 
     ### H_salaire
     #Create table H_salaire
@@ -67,50 +67,50 @@ class data_base:
 
     # Insert value into H_salaire
     def insert_salaire(self,item):
-        self.cur.execute("""INSERT OR IGNORE INTO H_salaire VALUES(?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO H_salaire (salaire) VALUES(?)""", item)
 
     ### H_competanceCr eate table H_copetance
     #Create table H_copetence
     def create_competence(self):
         self.cur.execute("""CREATE TABLE IF NOT EXISTS H_competence(
-                         id_competence INTEGER PRIMARY KEY,
-                         competance TEXT
+                         id_competence INTEGER PRIMARY KEY AUTOINCREMENT,
+                         competence TEXT
         )""")
 
     # Insert velue into H_competance
     def insert_competance(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO H_competence VALUES(?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO H_competence (competence) VALUES(?)""", item)
 
     ### H_langue
     # Create table H_langue
     def create_langue(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS H_langue(
-                         id_langue INTEGER PRIMARY KEY,
+                         id_langue INTEGER PRIMARY KEY AUTOINCREMENT,
                          langue TEXT
         )""")
 
     # Insert value into H_langue
     def insert_langue(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO H_langue VALUES(?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO H_langue (langue) VALUES(?)""", item)
 
     ### H_departement
     #C reate table H_departement
     def create_depatement(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS H_departement(
-                         id_departement INTEGER PRIMARY KEY,
+                         id_departement INTEGER PRIMARY KEY AUTOINCREMENT,
                          departement TEXT
         )""")
 
     # Insert value into H_depatement
     def insert_departement(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO H_departement VALUES(?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO H_departement (departement) VALUES(?)""", item)
 
 
     ### D_entreprise
     # Create table D_entreprise
     def create_entreprise(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS D_entreprise(
-                         id_entreprise INTEGER PRIMARY KEY,
+                         id_entreprise INTEGER PRIMARY KEY AUTOINCREMENT,
                          id_type_job INTEGER ,
                          id_salaire INTEGER,
                          entreprise TEXT,
@@ -120,25 +120,25 @@ class data_base:
 
     # Insert value into D_entreprise
     def insert_entreprise(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO D_entreprise VALUES(?,?,?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO D_entreprise (id_type_job, id_salaire, entreprise) VALUES(?,?,?)""", item)
 
 
     ### D_date 
     # Create table D_date
     def create_date(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS D_date(
-                         id_date INTEGER PRIMARY KEY,
+                         id_date INTEGER PRIMARY KEY AUTOINCREMENT,
                          date TEXT
         )""")
 
     # Insert value sinto D_date
     def insert_date(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO D_date VALUES(?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO D_date (date) VALUES(?)""", item)
 
     ### D_description
     def create_description(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS D_description(
-                         id_description INTEGER PRIMARY KEY,
+                         id_description INTEGER PRIMARY KEY AUTOINCREMENT,
                          id_competence INTEGER,
                          id_langue INTEGER,
                          description TEXT,
@@ -147,34 +147,34 @@ class data_base:
         )""")
 
     def instert_description(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO D_description VALUES(?,?,?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO D_description (id_competence, id_langue, description) VALUES(?,?,?)""", item)
 
     ### D_source
     def create_source(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS D_source(
-                         id_source INTEGER PRMIMARY KEY,
+                         id_source INTEGER PRIMARY KEY AUTOINCREMENT,
                          source TEXT
         )""")
 
-        def insert_source(self, item):
-            self.cur.execute("""INSERT OR IGNORE INTO D_source VALUES(?,?)""", item)
+    def insert_source(self, item):
+        self.cur.execute("""INSERT OR IGNORE INTO D_source (source) VALUES(?)""", item)
 
     ### D_location
     def create_location(self):
         self.con.execute("""CREATE TABLE IF NOT EXISTS D_location(
-                         id_location INTEGER PRIMARY KEY,
+                         id_location INTEGER PRIMARY KEY AUTOINCREMENT,
                          id_departement INTEGER,
                          ville TEXT,
                          FOREIGN KEY(id_departement) REFERENCES H_depatement(id_departement)
         )""")
 
     def insert_location(self, item):
-        self.cur.execute("""INSERT OR IGNORE INTO D_location VALUES(?,?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO D_location (id_departement, ville) VALUES(?,?)""", item)
 
     # F_titre
     def create_titre(self):
         self.cur.execute("""CREATE TABLE IF NOT EXISTS F_titre(
-                         id_titre INTEGER PRIMARY KEY,
+                         id_titre INTEGER PRIMARY KEY AUTOINCREMENT,
                          id_entreprise INTEGER,
                          id_date INTEGER,
                          id_description INTEGER,
@@ -189,10 +189,15 @@ class data_base:
         )""")
 
     def insert_titre(self,item):
-        self.cur.execute("""INSERT OR IGNORE INTO F_titre(?,?,?,?,?,?,?)""", item)
+        self.cur.execute("""INSERT OR IGNORE INTO F_titre (id_entreprise, id_date, id_description, id_source, id_location, titre) VALUES(?,?,?,?,?,?)""", item)
 
     def commit_change(self):
         self.con.commit()
 
     def close_connection(self):
         self.con.close()
+
+    def req(self, requete):
+        self.cur.execute(requete)
+        rows = self.cur.fetchall()
+        return rows
