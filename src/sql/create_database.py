3c4c28e9e6f24ea79ebe525589420ac636031a78
joepelham <104430977+joepelham@users.noreply.github.com>
2023-12-09 22:51:22 +0100

    Slightly different structure

--- src/sql/create_database.py
+++ src/sql/create_database.py
@@ -2,88 +2,101 @@ import sqlite3
 
 def create_database():
     # Connect to the SQLite database (or create it if it doesn't exist)
-    conn = sqlite3.connect('job_database.db')
+    conn = sqlite3.connect('jobs.db')
 
     # Create a cursor object to execute SQL commands
     cursor = conn.cursor()
 
-    # Create the dimension tables
-    create_title_table_sql = '''
-    CREATE TABLE IF NOT EXISTS d_title (
-        title_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT
-    );
-    '''
-    cursor.execute(create_title_table_sql)
-
-    create_type_table_sql = '''
-    CREATE TABLE IF NOT EXISTS d_type_job (
-        type_job_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        type_job TEXT
-    );
-    '''
-    cursor.execute(create_type_table_sql)
-
-    create_company_table_sql = '''
-    CREATE TABLE IF NOT EXISTS d_company (
-        company_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        company TEXT
-    );
-    '''
-    cursor.execute(create_company_table_sql)
-
-    create_source_table_sql = '''
-    CREATE TABLE IF NOT EXISTS d_source (
-        source_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        source TEXT
-    );
-    '''
-    cursor.execute(create_source_table_sql)
-
-    create_time_table_sql = '''
-    CREATE TABLE IF NOT EXISTS d_time (
-        time_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        date TEXT
-    );
-    '''
-    cursor.execute(create_time_table_sql)
-
-    create_location_table_sql = '''
-    CREATE TABLE IF NOT EXISTS d_location (
-        location_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        ville TEXT,
-        departement_id INTEGER,
-        FOREIGN KEY (departement_id) REFERENCES h_department(departement_id)
-    );
-    '''
-    cursor.execute(create_location_table_sql)
-
-    create_department_table_sql = '''
-    CREATE TABLE IF NOT EXISTS h_department (
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS H_department (
         departement_id INTEGER PRIMARY KEY AUTOINCREMENT,
         departement TEXT,
         region TEXT
-    );
-    '''
-    cursor.execute(create_department_table_sql)
+        )''')
+
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_location (
+        location_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        location TEXT,
+        departement_id INTEGER,
+        FOREIGN KEY (departement_id) REFERENCES H_department(departement_id)
+        )''')
+    
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_salary (
+        salary_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        salary TEXT
+        )''')
+    
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS H_type_job(
+        id_type_job INTEGER PRIMARY KEY AUTOINCREMENT,
+        type_job TEXT
+        )''')
+        
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_company (
+        company_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        company TEXT,
+        id_type_job INTEGER, 
+        FOREIGN KEY (id_type_job) REFERENCES H_type_job(id_type_job)
+        )''')
+
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_source (
+        source_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        source TEXT
+        )''')
+
+
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_date (
+        date_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT
+        )''')
+    
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_competences (
+        competence_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        competence TEXT
+        )''')
+    
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS H_language (
+        language_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        language TEXT
+        )''')
+
+
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_description (
+        description_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        language_id INTEGER,
+        FOREIGN KEY (language_id) REFERENCES H_language(language_id)
+        )''')
+    
+    
 
     # Create the "Fact" table with foreign key references
     create_fact_table_sql = '''
-    CREATE TABLE IF NOT EXISTS Fact (
+    CREATE TABLE IF NOT EXISTS F_title (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title_id INTEGER,
-        type_job_id INTEGER,
         source_id INTEGER,
         company_id INTEGER,
-        time_id INTEGER,
+        date_id INTEGER,
         location_id INTEGER,
-        description TEXT,
-        FOREIGN KEY (title_id) REFERENCES d_title(title_id),
-        FOREIGN KEY (type_job_id) REFERENCES d_type_job(type_job_id),
-        FOREIGN KEY (source_id) REFERENCES d_source(source_id),
-        FOREIGN KEY (company_id) REFERENCES d_company(company_id),
-        FOREIGN KEY (time_id) REFERENCES d_time(time_id),
-        FOREIGN KEY (location_id) REFERENCES d_location(location_id)
+        salary_id INTEGER,
+        competence_id INTEGER,
+        description_id INTEGER,
+        title TEXT,
+        FOREIGN KEY (source_id) REFERENCES D_source(source_id),
+        FOREIGN KEY (company_id) REFERENCES D_company(company_id),
+        FOREIGN KEY (salary_id) REFERENCES D_salary(salary_id),
+        FOREIGN KEY (date_id) REFERENCES D_date(date_id),
+        FOREIGN KEY (location_id) REFERENCES D_location(location_id),
+        FOREIGN KEY (competence_id) REFERENCES D_competences(competence_id),
+        FOREIGN KEY (description_id) REFERENCES D_description(description_id)
     );
     '''
     cursor.execute(create_fact_table_sql)
@@ -91,7 +104,6 @@ def create_database():
     # Commit the changes and close the connection
     conn.commit()
     conn.close()
+    
 
-# Run the function to create the database and dimension tables
 create_database()
-
