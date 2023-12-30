a3c5dc1da10432c86d295f23a2bf910d9aa50ed9
joepelham <104430977+joepelham@users.noreply.github.com>
2023-12-30 18:55:17 +0100

    Update 30/12

--- src/sql/insert_functions.py
+++ src/sql/insert_functions.py
@@ -10,6 +10,17 @@ def insert_dimension(table_name, column_name, value, cursor):
         cursor.execute(f"INSERT INTO {table_name} ({column_name}) VALUES (?)", (value,))
         cursor.execute(f"SELECT last_insert_rowid() AS {column_name}_id")
         return cursor.fetchone()[0]
+    
+# Function to insert data into a dimension table with value check
+def insert_departement(departement, region, cursor):
+    cursor.execute(f"SELECT departement_id FROM H_departement WHERE departement = ?", (departement,))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    else:
+        cursor.execute(f"INSERT INTO H_departement (departement,region) VALUES (?,?)", (departement,region))
+        cursor.execute(f"SELECT last_insert_rowid() AS departement_id")
+        return cursor.fetchone()[0]
 
     
 def insert_observations(df, cursor):
@@ -22,57 +33,42 @@ def insert_observations(df, cursor):
         salary = row['salary']
         company = row['compagny']
         location = row['location']
-        language = row['language']
+        region = row['region']
+        departement = row['departement']
+        latitude = row['latitude']
+        longitude = row['longitude']
+        experience = row['experience']
         skills = row['skills']
         date = row['date']
         description = row['description']
-        # source = row['source']
+        tokens = row['tokens']
+        source = row['source']
 
         # Insert Observation
-        type_job_id_1 = insert_dimension('H_type_job', 'type_job', type_job, cursor)
-        language_id_1 = insert_dimension('H_language', 'language', language, cursor)
-        competence_id_1 = insert_dimension('H_competences', 'competence', skills, cursor)
-        print(type(type_job_id_1))
-
-        #print(f"type_job_id_1: {type_job_id_1}")
-        # source_id_1 = insert_dimension('D_source', 'source', source, cursor)
+        departement_id_1 = insert_departement(departement, region, cursor)
+        
+        skills_id_1 = insert_dimension('D_skills', 'skills', skills, cursor)
+        experience_id_1 = insert_dimension('D_experience', 'experience', experience, cursor)
+        tokens_id_1 = insert_dimension('D_tokens', 'tokens', tokens, cursor)
+        source_id_1 = insert_dimension('D_source', 'source', source, cursor)
         company_id_1 = insert_dimension('D_company', 'company', company, cursor)
+        type_job_id_1 = insert_dimension('D_type_job', 'type_job', type_job, cursor)
         salary_id_1 = insert_dimension('D_salary', 'salary', salary, cursor)
         date_id_1 = insert_dimension('D_date', 'date', date, cursor)
         location_id_1 = insert_dimension('D_location', 'location', location, cursor)
-        description_id_1 = insert_dimension('D_description', 'description', description, cursor)
+        title_id_1 = insert_dimension('D_title', 'title', title, cursor)
         
-        # cursor.execute("INSERT INTO D_company (type_job_id)""VALUES (?)",(type_job_id_1))
-        # cursor.execute("INSERT INTO D_description (language_id)""VALUES (?)",(language_id_1))
-        # cursor.execute("INSERT INTO D_description (competence_id)""VALUES (?)",(competence_id_1))
+        # cursor.execute(f"INSERT INTO D_location (departement_id) VALUES (?)", (departement_id_1))
         
-        cursor.execute("INSERT INTO F_title (company_id, date_id, location_id, description_id, salary_id, title) "
-                       "VALUES (?, ?, ?, ?, ?, ?)",
-                       (company_id_1, date_id_1, location_id_1, description_id_1, salary_id_1, title))
+        cursor.execute("INSERT INTO F_description (company_id, type_job_id, date_id, location_id, title_id, salary_id, skills_id, experience_id, tokens_id, description) "
+                       "VALUES (?, ?, ?, ?, ?, ?,?,?,?,?)",
+                       (company_id_1, type_job_id_1 ,date_id_1, location_id_1, title_id_1, salary_id_1,skills_id_1, experience_id_1, tokens_id_1, description))
     
     # conn.commit()
     # conn.close()
+    
+
+
 
     
     
-    
-# old
-def insert_observations1(values_list, cursor):
-
-    # Extract values from the list
-    title, type_job, salary, compagny, location, language, skills, date, description, source = values_list
-    
-    # Insert Observation
-    type_job_id_1 = insert_dimension('D_type_job', 'type_job', type_job, cursor)
-    source_id_1 = insert_dimension('D_source', 'source', source, cursor)
-    company_id_1 = insert_dimension('D_company', 'company', compagny, cursor)
-    time_id_1 = insert_dimension('D_date', 'date', date, cursor)
-    location_id_1 = insert_dimension('D_location', 'ville', location, cursor)
-    description_id_1 = insert_dimension('D_description', 'description', description, cursor)
-
-    cursor.execute("INSERT INTO Fact (type_job_id, source_id, company_id, date_id, location_id, description_id, salary, title) "
-                   "VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                   (type_job_id_1, source_id_1, company_id_1, time_id_1, location_id_1, description_id_1, salary, title))
-    
-    # conn.commit()
-    # conn.close()
