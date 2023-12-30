ac1bbec3241a4c5170137d349969fdc84b6970a0
joepelham <104430977+joepelham@users.noreply.github.com>
2023-12-30 18:54:42 +0100

    Update 30/12

--- src/sql/create_database.py
+++ src/sql/create_database.py
@@ -8,7 +8,7 @@ def create_database():
     cursor = conn.cursor()
 
     cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS H_department (
+        '''CREATE TABLE IF NOT EXISTS H_departement (
         departement_id INTEGER PRIMARY KEY AUTOINCREMENT,
         departement TEXT,
         region TEXT
@@ -19,7 +19,7 @@ def create_database():
         location_id INTEGER PRIMARY KEY AUTOINCREMENT,
         location TEXT,
         departement_id INTEGER,
-        FOREIGN KEY (departement_id) REFERENCES H_department(departement_id)
+        FOREIGN KEY (departement_id) REFERENCES H_departement(departement_id)
         )''')
     
     cursor.execute(
@@ -29,7 +29,7 @@ def create_database():
         )''')
     
     cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS H_type_job(
+        '''CREATE TABLE IF NOT EXISTS D_type_job(
         type_job_id INTEGER PRIMARY KEY AUTOINCREMENT,
         type_job TEXT
         )''')
@@ -37,9 +37,7 @@ def create_database():
     cursor.execute(
         '''CREATE TABLE IF NOT EXISTS D_company (
         company_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        company TEXT,
-        type_job_id INTEGER, 
-        FOREIGN KEY (type_job_id) REFERENCES H_type_job(type_job_id)
+        company TEXT
         )''')
 
     cursor.execute(
@@ -56,49 +54,55 @@ def create_database():
         )''')
     
     cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS H_competences (
-        competence_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        competence TEXT
+        '''CREATE TABLE IF NOT EXISTS D_skills (
+        skills_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        skills TEXT
         )''')
     
     cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS H_language (
-        language_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        language TEXT
-        )''')
-
-
-    cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS D_description (
-        description_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT,
-        language_id INTEGER,
-        competence_id INTEGER,
-        FOREIGN KEY (language_id) REFERENCES H_language(language_id),
-        FOREIGN KEY (competence_id) REFERENCES H_competences(competence_id)
+        '''CREATE TABLE IF NOT EXISTS D_title (
+        title_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT
         )''')
     
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_experience (
+        experience_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        experience TEXT
+        )''')
+    
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS D_tokens (
+        tokens_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tokens TEXT
+        )''')
     
 
     # Create the "Fact" table with foreign key references
     create_fact_table_sql = '''
-    CREATE TABLE IF NOT EXISTS F_title (
+    CREATE TABLE IF NOT EXISTS F_description (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         source_id INTEGER,
         company_id INTEGER,
+        type_job_id INTEGER,
         date_id INTEGER,
         location_id INTEGER,
         salary_id INTEGER,
-        competence_id INTEGER,
-        description_id INTEGER,
-        title TEXT,
+        skills_id INTEGER,
+        title_id INTEGER,
+        experience_id INTEGER,
+        tokens_id INTEGER,
+        description TEXT,
         FOREIGN KEY (source_id) REFERENCES D_source(source_id),
         FOREIGN KEY (company_id) REFERENCES D_company(company_id),
+        FOREIGN KEY (type_job_id) REFERENCES D_type_job(type_job_id),
         FOREIGN KEY (salary_id) REFERENCES D_salary(salary_id),
         FOREIGN KEY (date_id) REFERENCES D_date(date_id),
         FOREIGN KEY (location_id) REFERENCES D_location(location_id),
-        FOREIGN KEY (competence_id) REFERENCES D_competences(competence_id),
-        FOREIGN KEY (description_id) REFERENCES D_description(description_id)
+        FOREIGN KEY (skills_id) REFERENCES D_skills(skills_id),
+        FOREIGN KEY (title_id) REFERENCES D_title(title_id),
+        FOREIGN KEY (experience_id) REFERENCES D_experience(experience_id),
+        FOREIGN KEY (tokens_id) REFERENCES D_tokens(tokens_id)
     );
     '''
     cursor.execute(create_fact_table_sql)
